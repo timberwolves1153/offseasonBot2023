82548702639eec91c22ed18beeec1c5bd2e0539a
walpolerobotics <roborebels1153@gmail.com>
2023-11-02 20:35:03 -0400

    Started autos

---
+++ src/main/java/frc/robot/autos/PPAutoBase.java
@@ -0,0 +1,47 @@
+package frc.robot.autos;
+
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+public class PPAutoBase extends SequentialCommandGroup {
+    private Swerve swerve;
+    
+    public PIDController thetaController = new PIDController(Constants.AutoConstants.kPThetaController, 
+        0, 0);
+
+    public PPAutoBase(Swerve swerve) {
+        this.swerve = swerve;
+        addRequirements(swerve);
+    }
+
+    public SequentialCommandGroup followTrajectoryCommand(PathPlannerTrajectory path1, boolean isFirstPath) {
+        PIDController thetaController = new PIDController(3, 0, 0);
+        PIDController xController = new PIDController(3, 0, 0);
+        PIDController yController = new PIDController(3, 0, 0);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+            return new SequentialCommandGroup(
+                new InstantCommand(() -> {
+                    if(isFirstPath) {
+                        swerve.resetOdometry(transformed.getInitialHolonomicMode());
+                    }
+                })),
+                new PPSwerveControllerCommand(
+                 path1,
+                 swerve::getPose, 
+                 Constants.Swerve.swerveKinematics, 
+                 xController, 
+                 yController, 
+                 thetaController, 
+                 swerve::setModuleStates,  
+                 true, 
+                 swerve 
+             )
+             .andThen(() -> swerve.stop());
+     }
+}
+
---
+++ src/main/java/frc/robot/autos/ScoreAndMobility.java
@@ -0,0 +1,19 @@
+package frc.robot.autos;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import frc.robot.subsystems.Collector;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Swerve;
+
+public class ScoreAndMobility extends PPAutoBase {
+
+    public void ScoreandMobility(Swerve swerve) {
+
+        PathPlannerTrajectory ScoreandMobility = PathPlanner.loadAuto("Scoreand")
+
+    } 
+
+}
+
